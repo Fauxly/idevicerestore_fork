bd803bb7bace16089131ecbe59054a84203b7ab8
Fauxly <tailormoon@icloud.com>
2026-07-25 07:52:02 +0300

    idevicerestore: fix wait loop condition that regressed the restore-mode wait to ~0ms

--- src/idevicerestore.c
+++ src/idevicerestore.c
@@ -3047,14 +3047,18 @@ value = NULL; \
     logger(LL_INFO, "Waiting for device to enter restore mode...\n");
 
     {
-        int total_waited = 0;
-        const int step_ms = 5000;
-        const int max_wait_ms = 100000000;
-        while (client->mode == MODE_UNKNOWN && !(client->flags & FLAG_QUIT) && total_waited < max_wait_ms) {
-            cond_wait_timeout(&client->device_event_cond, &client->device_event_mutex, step_ms);
-            total_waited += step_ms;
-        }
+    int total_waited = 0;
+    const int step_ms = 5000;
+    const int max_wait_ms = 100000000;
+    while (client->mode != MODE_RESTORE &&
+           client->mode != MODE_DFU &&
+           client->mode != MODE_PONGO &&
+           !(client->flags & FLAG_QUIT) &&
+           total_waited < max_wait_ms) {
+        cond_wait_timeout(&client->device_event_cond, &client->device_event_mutex, step_ms);
+        total_waited += step_ms;
     }
+}
 #ifdef HAVE_TURDUS_MERULA
 		if ((client->flags & FLAG_DOWNGRADE) && is_arm64_soc(client->cpid)) {
 			plist_t my_tss = NULL;
